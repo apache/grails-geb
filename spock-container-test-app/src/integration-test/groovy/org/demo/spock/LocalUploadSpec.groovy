c98faa4d2d2b24a334fe34c32bb95979e4eb65dd
Jonas Pammer <opensource@jonaspammer.at>
2025-02-22 23:22:51 +0100

    failing test

---
+++ spock-container-test-app/src/integration-test/groovy/org/demo/spock/LocalUploadSpec.groovy
@@ -0,0 +1,43 @@
+package org.demo.spock
+
+import grails.plugin.geb.ContainerGebSpec
+import grails.testing.mixin.integration.Integration
+import org.demo.spock.pages.UploadPage
+import spock.lang.IgnoreIf
+import spock.lang.Requires
+
+@Integration
+class LocalUploadSpec extends ContainerGebSpec {
+
+    @Requires({ os.windows })
+    void 'should be able to find and upload files on a Windows host'() {
+        given:
+        def uploadPage = to UploadPage
+
+        when:
+        uploadPage.fileInput.file = new File("src/integration-test/resources/assets/upload-test.txt")
+
+        and:
+        uploadPage.submitBtn.click()
+
+        then:
+        title == 'File Uploaded'
+        pageSource.contains('File uploaded successfully')
+    }
+
+    @IgnoreIf({ os.windows })
+    void 'should be able to find and upload files on a non-Windows host'() {
+        given:
+        def uploadPage = to UploadPage
+
+        when:
+        uploadPage.fileInput.file = new File("src/integration-test/resources/assets/upload-test.txt")
+
+        and:
+        uploadPage.submitBtn.click()
+
+        then:
+        title == 'File Uploaded'
+        pageSource.contains('File uploaded successfully')
+    }
+}
